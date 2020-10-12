5af78dc0163da03bf50339b0909087c9084e5606
pushsla <push2001sla@gmail.com>
2020-10-12 23:52:30 +0300

    Automatical generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::fmt::Display;
 use std::time::Instant;
+use rand::Rng;
 
 fn read_matrix(n: i64) -> Vec<Vec<i64>>{
     let mut result = Vec::new();
@@ -55,7 +56,7 @@ fn shaker_sort<T: Ord + Clone>(vec: &Vec<T>) -> Vec<T>{
     let mut l = 1;
     let mut finished = false;
 
-    while (!finished){
+    while !finished{
         finished = true;
         for i in (l-1)..(vec.len()-r){
             if vec[i] > vec[i+1]{
@@ -84,12 +85,10 @@ fn shaker_sort<T: Ord + Clone>(vec: &Vec<T>) -> Vec<T>{
 fn selection_sort<T: Ord + Clone>(vec: &Vec<T>) -> Vec<T>{
     let mut vector = vec.to_vec();
     let mut result = Vec::new();
-    let mut min = &vector[0];
-    let mut min_index = 0;
 
     for _i in 0..vec.len(){
-        min = &vector[0];
-        min_index = 0;
+        let mut min = &vector[0];
+        let mut min_index = 0;
         for i in 0..vector.len(){
             if vector[i] < *min{
                 min = &vector[i];
@@ -104,26 +103,26 @@ fn selection_sort<T: Ord + Clone>(vec: &Vec<T>) -> Vec<T>{
 }
 
 fn main(){
-    //let numbers = read_matrix(1)[0].to_vec();
-    //let mut letters = String::new();
+    let mut rng = rand::thread_rng();
 
-    //io::stdin().read_line(&mut letters).unwrap();
-    let numbers = vec![0,2,3,6,5,4,8,9,8,6,5,4,3,4,5,6,7,6,1,2,3,0,9,9,1,7,2,3,6,5,4,8,4,0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1];
-    let letters = "q a z x s w e d c v f r t g b n h y u j m k i o p l p o i u y t r e w q a s d f g h j k l m n b v c x z a b c d e f g h";
-    let letters = letters.trim().split(" ").collect();
+    let items_count: usize = 10000;
+    let original_numbers: Vec<usize> = (0..items_count).collect();
+    let mut numbers = original_numbers.to_vec();
+
+    for i in 0..items_count{
+        numbers.swap(rng.gen_range(0, items_count), i);
+    }
+
+    println!("Start sorting!");
+    let now = Instant::now();
+    bubble_sort(&numbers);
+    println!("Bubble: Time (microsecs): {}; (secs): {}", now.elapsed().as_micros(), now.elapsed().as_secs());
 
     let now = Instant::now();
-    print_vector(&bubble_sort(&numbers));
-    print_vector(&bubble_sort(&letters));
-    println!("Bubble: Time (microsecs): {}", now.elapsed().as_micros());
+    shaker_sort(&numbers);
+    println!("Shaker: Time (microsecs): {}; (secs): {}", now.elapsed().as_micros(), now.elapsed().as_secs());
 
     let now = Instant::now();
-    print_vector(&shaker_sort(&numbers));
-    print_vector(&shaker_sort(&letters));
-    println!("Shaker: Time (microsecs): {}", now.elapsed().as_micros());
-
-    let now = Instant::now();
-    print_vector(&selection_sort(&numbers));
-    print_vector(&selection_sort(&letters));
-    println!("Selection: Time (microsecs): {}", now.elapsed().as_micros());
+    selection_sort(&numbers);
+    println!("Selection: Time (microsecs): {}; (secs): {}", now.elapsed().as_micros(), now.elapsed().as_secs());
 }
